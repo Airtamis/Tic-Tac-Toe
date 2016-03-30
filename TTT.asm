5aa6b01c15eefff0a38b817ae765bac939497685
Airtamis <jrggb@yahoo.com>
2016-03-30 18:16:36 -0500

    Added Win Conditons

--- TTT.asm
+++ TTT.asm
@@ -20,9 +20,8 @@ SECTION .bss
 	compSymb:	resd 1
 
 SECTION .data
-	badBounds:	db "ERROR: Invalid space", 10, 0 ;user chose a space that is not 1-9
-	;board:		times 9 dd 0	;legit board
-	board: 		dd 0, 1, 2, 0, 1, 2, 0, 1, 2	; test board
+	m_badBounds:	db "ERROR: Invalid space", 10, 0 ;user chose a space that is not 1-9
+	board:		times 9 dd 0	;legit board
 	f_int: 		db "%d", 0 		; Integer format
 	f_strn:		db "%s", 10, 0 		; String format w newline
 	f_str:		db "%s", 0		; String format
@@ -36,12 +35,12 @@ SECTION .data
 	X:		db "X", 0		; X
 	O:		db "O", 0		; Y
 	E:		db " ", 0		; Space
-	badSpot:	db "Space already occupied", 10, 0 ; User chose occupied spot in board
+	m_badSpot:	db "Space already occupied", 10, 0 ; User chose occupied spot in board
 	newline:	db 10, 0		; newline character
-	tieWin:		db "Tie game!", 10, 0	; Tie Game Message
-	compWin:	db "The Computer Won, of course!", 10, 0 ; Computer Win Message
-	playWin:	db "Crap, the player won...", 10, 0	; Player win message
-
+	m_tieWin:	db "Tie game!", 10, 0	; Tie Game Message
+	m_compWin:	db "The Computer Won, of course!", 10, 0 ; Computer Win Message
+	m_playWin:	db "Crap, the player won...", 10, 0	; Player win message
+	m_here:		db "HERE\n", 0
 SECTION .text
 	global main
 	extern scanf
@@ -51,10 +50,24 @@ SECTION .text
 
 main:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; BEGIN main
 	;jmp exit
-	
+
+	push white
+	call printf
+	add ESP, 4
+
+	push black
+	call printf
+	add ESP, 4
+
+	mov [playSymb], DWORD X_VAL
+
+.top:
 	call printBoard 	; print original empty board
-	call getInput		; get user input for move placement
-	call printBoard		; print updated board after first move
+	call getInput
+	call calcWin
+
+	cmp EAX, 0
+	je .top
 
 	jmp exit		; exit program
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; END main
@@ -68,15 +81,15 @@ getInput:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; BEGIN getInput
 	mov EAX, [input]	; move user's inputted number into EAX
 
 	cmp EAX, 1		; make sure user's input is greater or equal to 1
-	jl	.fail1		; if not, print badBounds
+	jl	.fail1		; if not, print m_badBounds
 	cmp EAX, 9		; make sure user's input is less than or equal to 9
-	jg	.fail1		; if not, print badBounds
+	jg	.fail1		; if not, print m_badBounds
 
 	call setSpot		; if input is within bounds, attempt to set space
 
 	jmp .end		; return
 .fail1:
-	push badBounds		; push out of bounds warning string to stack
+	push m_badBounds		; push out of bounds warning string to stack
 	push f_str		; push string format string to stack
 	call printf		; print warning to user
 	add ESP, 8		; adjust stack pointer
@@ -99,7 +112,7 @@ setSpot:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; BEGIN setSpot
 	mov [board + EAX], EBX  ; set board spot to player's symbol
 	jmp .end		; ret
 .bad:
-	push badSpot		; move warning string that space is occupied to stack
+	push m_badSpot		; move warning string that space is occupied to stack
 	call printf		; print warning to user
 	add ESP, 4		; adjust stack pointer
 
@@ -109,14 +122,6 @@ setSpot:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; BEGIN setSpot
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; END setSpot
 
 printBoard:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; BEGIN printBoard
-
-	push white
-	call printf
-	add ESP, 4
-
-	push black
-	call printf
-	add ESP, 4	
 
 	xor ECX, ECX		; set ECX to 0
 .top:	
@@ -175,24 +180,18 @@ printBoard:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; BEGIN printBoard
 	jmp .top
 
 .end:
-	push reset		; Return keyboard to normal
-	call printf		; Call printf
-	add ESP, 4		; Adjust stack pointer
-
 	ret
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; END printBoard
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; EAX = 0 if no win, 1 if computer win, 2 if player win, 3 if tie
 calcWin:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Begin calcWin
-	;; Board looks like:
-	;; 0, 1, 2
-	;; 3, 4, 5
-	;; 6, 7, 8
-
-	call checkXWin
+	mov EBX, X_VAL
+	call checkWin
 	cmp EAX, 1		; If X won, EAX will be 1
 	je .XWin
-	call checkOWin
+
+	mov EBX, O_VAL
+	call checkWin
 	cmp EAX, 1		; If O won, EAX will be 2
 	je .OWin
 	call checkTie
@@ -201,12 +200,35 @@ calcWin:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Begin calcWin
 	jmp .noWin
 	 
 .XWin:
+	mov EAX, [playSymb]
+	cmp EAX, X_VAL
+	je .playWin
+	jmp .compWin
 
 .OWin:
-	mov playSmb
+	mov EAX, [playSymb]
+	cmp EAX, O_VAL
+	je .playWin
+	jmp .compWin
+
+.compWin:
+	push m_compWin
+	call printf
+	add ESP, 4
+
+	mov EAX, 1
+	jmp .end
+
+.playWin:
+	push m_playWin
+	call printf
+	add ESP, 4
+	
+	mov EAX, 2
+	jmp .end
 
 .tie:
-	push tie
+	push m_tieWin
 	call printf
 	add ESP, 4
 	
@@ -214,25 +236,171 @@ calcWin:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Begin calcWin
 	jmp .end
 
 .noWin:
+	push newline
+	call printf
+	add ESP, 4
+
 	xor EAX, EAX
 	jmp .end
 
 .end:	ret
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; END calcWin
 
-checkXWin:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; BEGIN checkXWin
+checkWin:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; BEGIN checkWin
 
+.topRow:
+	mov EAX, [board]	; Top left cell
+	cmp EAX, EBX
+	jne .centRow
+
+	mov EAX, [board + 4]	; Top center cell
+	cmp EAX, EBX
+	jne .centRow
+
+	mov EAX, [board + 8]	; Top right cell
+	cmp EAX, EBX
+	jne .centRow
+	jmp .win
+
+.centRow:
+	mov EAX, [board + 12]	; Middle left cell
+	cmp EAX, EBX
+	jne .botRow
+
+	mov EAX, [board + 16]	; Middle center cell
+	cmp EAX, EBX
+	jne .botRow
+
+	mov EAX, [board + 20]	; Middle right cell
+	cmp EAX, EBX
+	jne .botRow
+	jmp .win
+
+.botRow:
+	mov EAX, [board + 24]	; Middle left cell
+	cmp EAX, EBX
+	jne .leftCol
+
+	mov EAX, [board + 28]	; Middle center cell
+	cmp EAX, EBX
+	jne .leftCol
+
+	mov EAX, [board + 32]	; Middle right cell
+	cmp EAX, EBX
+	jne .leftCol
+	jmp .win
+
+
+.leftCol:
+	mov EAX, [board]	; Middle left cell
+	cmp EAX, EBX
+	jne .centCol
+
+	mov EAX, [board + 12]	; Middle center cell
+	cmp EAX, EBX
+	jne .centCol
+
+	mov EAX, [board + 24]	; Middle right cell
+	cmp EAX, EBX
+	jne .centCol
+	jmp .win
+
+.centCol:
+	mov EAX, [board + 4]	; Middle left cell
+	cmp EAX, EBX
+	jne .rightCol
+
+	mov EAX, [board + 16]	; Middle center cell
+	cmp EAX, EBX
+	jne .rightCol
+
+	mov EAX, [board + 28]	; Middle right cell
+	cmp EAX, EBX
+	jne .rightCol
+	jmp .win
+
+
+.rightCol:
+	mov EAX, [board + 8]	; Middle left cell
+	cmp EAX, EBX
+	jne .lrDi
+
+	mov EAX, [board + 20]	; Middle center cell
+	cmp EAX, EBX
+	jne .lrDi
+
+	mov EAX, [board + 32]	; Middle right cell
+	cmp EAX, EBX
+	jne .lrDi
+	jmp .win
+
+.lrDi:
+	mov EAX, [board]	; Middle left cell
+	cmp EAX, EBX
+	jne .rlDi
+
+	mov EAX, [board + 16]	; Middle center cell
+	cmp EAX, EBX
+	jne .lrDi
+
+	mov EAX, [board + 32]	; Middle right cell
+	cmp EAX, EBX
+	jne .lrDi
+	jmp .win
+
+
+.rlDi:
+	mov EAX, [board + 8]	; Middle left cell
+	cmp EAX, EBX
+	jne .fail
+
+	mov EAX, [board + 16]	; Middle center cell
+	cmp EAX, EBX
+	jne .fail
+
+	mov EAX, [board + 24]	; Middle right cell
+	cmp EAX, EBX
+	jne .fail
+	jmp .win
+
+
+.fail:
+	xor EAX, EAX
+	jmp .end
+.win:
+	mov EAX, 1
+	jmp .end
+	
 .end:	ret
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; END checkXWin
-
-checkOWin:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; BEGIN checkOWin
-
-.end:	ret
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; END checkOWin
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; END checkWin
 
 checkTie:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; BEGIN checkTie
+	xor ECX, ECX
 
-.end:	ret
+.top:
+	cmp ECX, 9
+	je .end
+	
+	mov EAX, ECX		; Put ECX in EAX
+	xor EDX, EDX		; Zero out EDX
+	imul EAX, 4		; Multiply by size of DWORD
+
+	mov EBX, [board + EAX]	; Put spot in array into EBX
+	
+	cmp EBX, 0
+	je .empty
+	jmp .bottom
+
+.bottom:
+	inc ECX
+	jmp .top
+
+.empty:				; If there's an esmpty space
+	xor EAX, EAX
+	ret
+.end:	
+	mov EAX, 1
+	ret
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; END checkTie
 
 exit:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; BEGIN exit
